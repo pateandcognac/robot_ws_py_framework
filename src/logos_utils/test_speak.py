febabc82c7c5b48eaae8a5e0d487bac9d4993e75
pateandcognac <pateandcognac@gmail.com>
2026-05-22 10:23:04 -0400

    Add new wake word models and documentation for various characters and phrases
    - Created wake word models for "Hey Frenck", "Hey Guillermo", "Hey KITT", "Hey Miriel", "Hey Nabu", "Hey Potato", "Hey Rick", "Hey Santa", "Hey Snips", "Hey Spock", "Hi 小问", "Jarvis", "Johnny Five", "Kelsey", "Marvin", "Mirror mirror on the wall", "Mr. Smith", "你好米雅", "你好问问", "Oi Fuckwhit", "Ok Paulus", "Ok Tau", "Pandora", "R2-D2", "Rubber Duck", "Sheila", "WALL-E", "Wheatley", and "Yo Homie".
    - Added training details and results for each model in their respective README files.
    - Included a LICENSE file for the openwakeword-feature-models directory.

--- src/logos_utils/test_speak.py
+++ src/logos_utils/test_speak.py
@@ -107,17 +107,18 @@ def test_client():
     (us1_mbrola kal_diphone us3_mbrola don_diphone ked_diphone rab_diphone us2_mbrola en1_mbrola cmu_us_slt_arctic_hts)
     """
 
+
     # Construct the goal
     goal = SpeakGoal()
     goal.utterance_text = story_text
-    goal.engine = "festival"  # Options: "espeak", "kokoro", "piper"
+    goal.engine = "kokoro"  # Options: "espeak", "kokoro", "piper"
 
 
-    params = {"voice": "us2_mbrola", "speed": 1.00, "volume": 1.0} 
+    # params = {"voice": "us2_mbrola", "speed": 1.00, "volume": 1.2} 
     # params = {"voice": "default+croak", "speed": 1.0, "volume": 1.0} # espeak
     # params = {"voice": "en-us+m7", "speed": 1.1, "volume": 1.0} # espeak
     # params = {"voice": "en-wm", "speed": 0.85, "volume": 1.0} # espeak
-    # params = {"voice": "0.3*am_onyx + 0.2*im_nicola + 0.05*bf_isabella + 0.05*hf_alpha + 0.05*em_santa + 0.05*ff_siwis + 0.05*zm_yunjian + 0.05*jf_nezumi + 0.5*pm_alex + 0.05*hm_omega + 0.05*jf_alpha + 0.05*zf_xiaoni", "speed": 1.275, "volume": 1.0} # kokoro
+    params = {"voice": "0.3*am_onyx + 0.2*im_nicola + 0.05*bf_isabella + 0.05*hf_alpha + 0.05*em_santa + 0.05*ff_siwis + 0.05*zm_yunjian + 0.05*jf_nezumi + 0.5*pm_alex + 0.05*hm_omega + 0.05*jf_alpha + 0.05*zf_xiaoni", "speed": 1.275, "volume": 1.0} # kokoro
     # params = {"voice": "0.3*am_onyx + 0.1*im_nicola + 0.05*bf_isabella + 0.1*hf_alpha + 0.05*em_santa + 0.05*ff_siwis + 0.1*zm_yunjian + 0.05*jf_nezumi + 0.05*pm_alex + 0.05*hm_omega + 0.05*jf_alpha + 0.05*zf_xiaoni", "speed": 1.275, "volume": 1.0} # kokoro
     # params = {"voice": "0.85*am_onyx + 0.15*bf_alice", "speed": 1.25, "volume": 1.0} # kokoro
     # params = {"voice": "0.25*am_michael + 0.25*am_echo + 0.5*am_onyx", "speed": 1.3, "volume": 1.0} # kokoro
---
+++ wakewords/home-assistant-wakewords-collection/README.md
@@ -0,0 +1,25 @@
+![banner_wwc](https://github.com/fwartner/home-assistant-wakewords-collection/assets/6692500/afa694ab-7329-4a13-a83c-e8f94ca3bcb0)
+
+# Home Assistant Wake Word Collection
+
+Welcome to the community-driven collection of wake words for Home Assistant using [openWakeWord](https://github.com/dscripka/openWakeWord) wake word detection system!
+
+## Easy Install
+Check out [WakeWords Installer](https://github.com/fwartner/home-assistant-wakeword-installer)!
+
+## Usage Guidelines
+To incorporate any of the wake words into your Home Assistant setup, please refer to the step-by-step instructions available in the [Home Assistant documentation](https://www.home-assistant.io/voice_control/create_wake_word/?ref=wartner.io#to-add-your-personal-wake-word-to-home-assistant).
+
+## Contribution Instructions
+If you'd like to contribute your own wake words, simply follow the provided [instructions](https://www.home-assistant.io/voice_control/create_wake_word/) to train new wake word models using Colab.
+
+## Security Concerns
+For any security-related concerns, please reach out to me via email at florian@wartner.io, and I will address them promptly.
+
+## Show Your Support
+If you appreciate this collection and would like to support my efforts, consider [sponsoring me on GitHub](https://github.com/sponsors/fwartner)!
+
+## Explore Related Projects
+Check out my other Home Assistant-related projects:
+
+- [My Home Assistant Configuration](https://github.com/fwartner/homeassistant-config)
